045c71b44e2e02047ab0499c3c36b95f9b128b47
Aaron Wilson <aaron.wilson6283@gmail.com>
2021-11-02 22:02:58 -0400

    Upadted Menu

--- Pop1_Stats_Collector/Pop1_Stats_Collector/UserInterfaces/StartingUserInterface.cs
+++ Pop1_Stats_Collector/Pop1_Stats_Collector/UserInterfaces/StartingUserInterface.cs
@@ -8,6 +8,33 @@ namespace Pop1_Stats_Collector.UserInterfaces
 {
     public static class StartingUserInterface
     {
+        public static bool Quit = false;
 
+        public static void CommandLoop()
+        {
+            while (!Quit)
+            {
+                Console.WriteLine(string.Empty);
+                Console.WriteLine("Enter from one of the following options:");
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("<C>hange current Username");
+                Console.WriteLine("<D>isplay current stats");
+                Console.WriteLine("<Q>uit");
+                var menu_selection = Console.ReadLine().ToUpper();
+                CommandPath(menu_selection);
+            }
+        }
+
+        public static void CommandPath(string menu_selection)
+        {
+            if (menu_selection == 'C')
+                ChangeUsername();
+            else if (menu_selection == 'D')
+                DisplayUserStats;
+            else if (menu_selection == 'Q')
+                Quit = true;
+            else
+                Console.WriteLine("{0} is not a valid command. Please try again.", menu_selection);
+        }
     }
 }
