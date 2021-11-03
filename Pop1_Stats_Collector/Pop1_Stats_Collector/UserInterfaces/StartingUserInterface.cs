cce49960ce4caed8b7db8920dd986737273997d3
Aaron Wilson <aaron.wilson6283@gmail.com>
2021-11-02 22:17:24 -0400

    formatting changes made

--- Pop1_Stats_Collector/Pop1_Stats_Collector/UserInterfaces/StartingUserInterface.cs
+++ Pop1_Stats_Collector/Pop1_Stats_Collector/UserInterfaces/StartingUserInterface.cs
@@ -10,7 +10,7 @@ namespace Pop1_Stats_Collector.UserInterfaces
     {
         public static bool Quit = false;
 
-        public static void CommandLoop()
+        public static void MenuLoop()
         {
             while (!Quit)
             {
@@ -20,6 +20,7 @@ namespace Pop1_Stats_Collector.UserInterfaces
                 Console.WriteLine("<C>hange current Username");
                 Console.WriteLine("<D>isplay current stats");
                 Console.WriteLine("<Q>uit");
+                Console.WriteLine(string.Empty);
                 var menu_selection = Console.ReadLine().ToUpper();
                 CommandPath(menu_selection);
             }
@@ -27,14 +28,18 @@ namespace Pop1_Stats_Collector.UserInterfaces
 
         public static void CommandPath(string menu_selection)
         {
-            if (menu_selection == 'C')
-                ChangeUsername();
-            else if (menu_selection == 'D')
-                DisplayUserStats;
-            else if (menu_selection == 'Q')
+            if (menu_selection == "C")
+                Quit = true;
+            else if (menu_selection == "D")
+                Quit = true;
+            else if (menu_selection == "Q")
                 Quit = true;
             else
+            {
+                Console.WriteLine(string.Empty);
                 Console.WriteLine("{0} is not a valid command. Please try again.", menu_selection);
+                Console.WriteLine(string.Empty);
+            }
         }
     }
 }
