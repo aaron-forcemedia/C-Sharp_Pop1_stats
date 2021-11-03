80e46c8762fc07ac17d3764052ebeaa469deaf12
Aaron Wilson <aaron.wilson6283@gmail.com>
2021-11-02 21:24:13 -0400

    First Commit

---
+++ Pop1_Stats_Collector/Pop1_Stats_Collector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pop1_Stats_Collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
